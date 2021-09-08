6ce208987a65e729cc826fd893337d484a07dc84
Jacob An <lightyear.thespaceranger@gmail.com>
2021-09-08 15:52:41 +0900

    Create classes

---
+++ Monster.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    public enum MonsterType
+    {
+        None = 0,
+        Slime = 1,
+        Orc = 2,
+        Skeleton = 3
+    }
+
+    class Monster : Creature
+    {
+        protected MonsterType type = MonsterType.None;
+
+        protected Monster(MonsterType type) : base(CreatureType.Monster) { this.type = type; }
+
+        public MonsterType GetMonsterType() { return this.type; }
+    }
+
+    class Slime : Monster
+    {
+        public Slime() : base(MonsterType.Slime)
+        {
+            SetInfo(10, 1);
+        }
+    }
+    class Orc : Monster
+    {
+        public Orc() : base(MonsterType.Slime) { SetInfo(20, 2); }
+    }
+    class Skeleton : Monster
+    {
+        public Skeleton() : base(MonsterType.Slime) { SetInfo(15, 5); }
+    }
+}
