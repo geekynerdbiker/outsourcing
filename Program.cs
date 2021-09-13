8475bb01ac5d856f49f404b14094b9420459e96f
Jacob An <lightyear.thespaceranger@gmail.com>
2021-09-13 11:40:37 +0900

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Game game = new Game();
+
+            while (true) game.Process();
+        }
+    }
+}
