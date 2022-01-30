e7d1f05a81ee207a0ed5bdb36a104e69b95e851f
Jacob An <lightyear.thespaceranger@gmail.com>
2022-01-31 03:19:11 +0900

    update

--- Done/py-json/main.py
+++ Done/py-json/main.py
@@ -19,9 +19,18 @@ class Databases:
     def commit(self):
         self.cursor.commit()
 
-    def insert(self, schema, table, column, data):
-        sql = " INSERT INTO {schema}.{table}({column}) VALUES ('{data}') ;".format(schema=schema, table=table,
-                                                                                   column=column, data=data)
+    def create(self, table, structure):
+        sql = "CREATE TABLE {table} ({structure}) ;".format(table=table, structure=structure)
+
+        try:
+            self.cursor.execute(sql)
+            self.db.commit()
+        except Exception as e:
+            print(" create DB  ", e)
+
+    def insert(self, table, column, data):
+        sql = "INSERT INTO {table}({column}) VALUES ({data}) ;".format(table=table,
+                                                                          column=column, data=data)
         try:
             self.cursor.execute(sql)
             self.db.commit()
@@ -34,16 +43,26 @@ file_path = 'task_metadata.json'
 with open(file_path, 'r', encoding='utf-8') as f:
     json_data = json.load(f)
 
-# db = Databases()
-# db.insert(schema='test', table='table', column=0, data='data')
-
-
+db = Databases()
 for keys in json_data.keys():
     print(f'[------ list name = {keys} ------]')
     i = 0
-    for data in json_data[keys]:
+    for datas in json_data[keys]:
         print(f'[--------- item {i + 1} ---------]')
         i += 1
-        s = str(data.values())
+        s = str(datas.values())
         s = s[12:len(s) - 1]
         print(s)
+
+        column, data = [], []
+        for key, value in datas.items():
+            column.append(key)
+            data.append(value)
+
+        column = str(column)
+        column = column.replace('[', '').replace('\'', '').replace(']', '')
+
+        data = str(data)
+        data = data.replace('[', '').replace(']', '')
+
+        db.insert(table=keys, column=column, data=data)
