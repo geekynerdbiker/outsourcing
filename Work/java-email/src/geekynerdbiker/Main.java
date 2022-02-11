c8fa2dec9814f881e767eff3c4c6b707a4e8db1d
Jacob An <lightyear.thespaceranger@gmail.com>
2022-02-12 05:40:30 +0900

    update

---
+++ Work/java-email/src/geekynerdbiker/Main.java
@@ -0,0 +1,20 @@
+package geekynerdbiker;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Please enter the total minutes to run: ");
+
+        System.out.println("Total number of messages processed: ");
+        System.out.println("Average arrival rate: ");
+        System.out.println("Average number of messages sent per minute: ");
+        System.out.println("Average number of messages in the queue per minute: ");
+        System.out.println("Number of messages sent on 1st attempt: ");
+        System.out.println("Number of messages sent on 2nd attempt: ");
+        System.out.println("Number of messages sent on 3rd attempt: ");
+        System.out.println("Number of messages sent on 4th attempt: ");
+        System.out.println("Number of messages sent on 5th attempt: ");
+        System.out.println("Average number of times messages had to be requeued: ");
+        // write your code here
+    }
+}
