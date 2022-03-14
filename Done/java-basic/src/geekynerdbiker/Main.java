52dd66bbbb5455445eef1bd066180fc649e40c55
Jacob An <lightyear.thespaceranger@gmail.com>
2022-03-15 00:27:14 +0900

    update

--- Done/java-basic/src/geekynerdbiker/Main.java
+++ Done/java-basic/src/geekynerdbiker/Main.java
@@ -4,24 +4,23 @@ import java.io.*;
 import java.util.StringTokenizer;
 
 public class Main {
-
     public static void main(String[] args) throws IOException {
-        // 파일 입력을 위한 Buffered Reader 및 File Reader 선언.
-        BufferedReader br = new BufferedReader(new FileReader("batch.inp"));
-        // 파일 출력을 위한 File Writer 선언.
-        FileWriter fw = new FileWriter("batch.out");
+        // 콘솔 입력을 위한 Buffered Reader 및 Input Stream Reader 선언.
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // 콘솔 출력을 위한 Buffered Writer 및 Output Stream Writer 선언.
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         // 문자열 토크나이징을 위한 String Tokenizer 선언.
         StringTokenizer st;
-        // 파일에서 첫 줄을 읽어서 정수형으로 변환한 후, p에 저장.
+        // 첫 줄을 읽어서 정수형으로 변환한 후, p에 저장.
         int p = Integer.parseInt(br.readLine());
         // CPU 유휴시간을 저장할 변수 CPU Rest, 처리가 종료된 시간을 저장할 변수 End Time 0으로 초기화.
         int CPURest = 0, endTime = 0;
         // 파일에서 읽어올 수 있는 줄이 없을 때까지 한 줄씩 읽기.
-        for (String line = br.readLine(); line != null; line = br.readLine()) {
+        for (int i = 0; i < p; i++) {
             // k가 홀수인지 짝수인지 판단하기 위한 변수 k, 0으로 초기화.
             int k = 0;
             // 읽어온 문자열을 공백(" ")을 기준으로 토큰화.
-            st = new StringTokenizer(line, " ");
+            st = new StringTokenizer(br.readLine(), " ");
             // 읽어온 문자열을 더이상 토큰화 할 수 없을 때까지 반복.
             while (st.hasMoreTokens()) {
                 // 토큰화 한 첫번째 숫자를 정수형으로 변환하여 변수 n에 저장.
@@ -40,8 +39,8 @@ public class Main {
             }
         }
         // 계산된 CPU 유휴시간과 처리가 종료된 시간을 파일에 씀.
-        fw.write(CPURest + " " + endTime);
+        bw.write(CPURest + " " + endTime);
         // 쓴 파일을 닫음. (닫지 않으면 파일에 쓰이지 않음)
-        fw.close();
+        bw.close();
     }
 }
