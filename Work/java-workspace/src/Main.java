4823a295f7122379db4f731bd4f2e2dc35f3e183
geekynerdbiker <lightyear.thespaceranger@gmail.com>
2023-11-03 16:34:59 +0900

    update

--- Work/java-workspace/src/Main.java
+++ Work/java-workspace/src/Main.java
@@ -1,10 +1,42 @@
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.awt.BorderLayout;
+import java.awt.Point;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
 public class Main {
-    public static final int SCREEN_WIDTH = 1280;
-    public static final int SCREEN_HEIGHT = 720;
+    private static Point point = new Point();
 
-    // 메인 함수
     public static void main(String[] args) {
-        new DynamicBeat();
-    }
+        final JFrame frame = new JFrame();
 
+        frame.setUndecorated(true);
+        JButton button = new JButton("Close Me");
+        button.addActionListener(e -> System.exit(0));
+        frame.addMouseListener(new MouseAdapter() {
+            public void mousePressed(MouseEvent e) {
+                point.x = e.getX();
+                point.y = e.getY();
+            }
+        });
+
+        frame.addMouseMotionListener(new MouseMotionAdapter() {
+            public void mouseDragged(MouseEvent e) {
+                Point p = frame.getLocation();
+                frame.setLocation(p.x + e.getX() - point.x, p.y + e.getY() - point.y);
+            }
+        });
+
+        frame.setSize(500, 500);
+        frame.setLocation(200, 200);
+        frame.setLayout(new BorderLayout());
+
+        frame.getContentPane().add(button, BorderLayout.NORTH);
+        frame.getContentPane().add(new JLabel("Drag Me", JLabel.CENTER),
+                BorderLayout.CENTER);
+        frame.setVisible(true);
+    }
 }
