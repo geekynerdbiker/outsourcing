20ac3e0f865b5abe71a731d1c4a955c34bf54bb9
Jacob An <lightyear.thespaceranger@gmail.com>
2023-12-17 21:57:12 +0900

    update

---
+++ Done/java-workspace/java-p2p_multi_chat/src/Client/Client2.java
@@ -0,0 +1,27 @@
+package src.Client;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+public class Client2 {
+    static boolean chatmode = false;
+    static int chatState = 0;
+
+    public static void main(String[] args) throws UnknownHostException, IOException {
+        try {
+            String ServerIP = "localhost";
+            Socket socket = new Socket(ServerIP, 8484);
+            System.out.println("Server connected");
+
+            Thread sender = new Sender(socket);
+            Thread receiver = new Receiver(socket);
+
+            sender.start();
+            receiver.start();
+
+        } catch (Exception e) {
+            System.out.println("Exception [MultiClient class]:" + e);
+        }
+    }
+}
